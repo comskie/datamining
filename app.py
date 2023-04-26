fb640627866c60823620bb0d5f99bc51bc2266ac
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-04-26 14:39:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,8 @@ def predict():
     # Get the data from the POST request.
     data = request.form.to_dict().values()
 
-    result = ShiftPredictor.get_instance().predict(data)
+    shift, result_percent = ShiftPredictor.get_instance().predict(data)
 
-    shift, result_percent = result
     message = 'You\'re good!' if not shift else 'You suck!'
 
     return render_template(
