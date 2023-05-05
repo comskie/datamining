fc931eb37cfe55eb55e6706f4c123932750f006d
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-05-05 15:13:20 +0800

    Create cleaner.py

---
+++ bin/cleaner.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+
+DATASET_PATH = os.path.join('..', os.getenv('DATASET_PATH', 'dataset.csv'))
+
+df = pd.read_csv(DATASET_PATH, keep_default_na=False)
+
+# Remove unnecessary columns
+dropFilter = df.filter(['Timestamp', 'Email Address', 'Participant\'s Agreement', 'ID Number', 'Year Level'])
+df = df.drop(dropFilter, axis=1)
+
+
+# Replace values
+df = df.replace(to_replace='95-90', value='100-90')
+df = df.replace(to_replace='TVL.*', value='TVL', regex=True)
+
+# Export cleaned dataset
+df.to_csv(DATASET_PATH, index=False)
+
+print('Done!')
